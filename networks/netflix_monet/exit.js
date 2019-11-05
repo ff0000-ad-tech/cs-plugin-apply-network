3f3760c4bc5708c5d3ed5ebced2019c49fff290b
Derek Miranda <derek.miranda.1992@gmail.com>
2019-11-04 16:24:45 -0800

    Updates Monet exit with v2 exit call

--- networks/netflix_monet/exit.js
+++ networks/netflix_monet/exit.js
@@ -1,8 +1,11 @@
 this.exit = function(clickTag, url) {
-	console.log('Network -> NETFLIX_MONET EXIT')
-	try {
-		Monet.logEvent('AD_EXIT', { url: url || 'DCS default exit' })
-	} catch (e) {}
-	if (typeof clickTag == 'function') clickTag.call(this, url)
-	else throw new Error('ERROR: clickTag for this platform needs to be a callable function. See <Network> docs.')
-}
+  console.log("Network -> NETFLIX_MONET EXIT");
+  try {
+    Monet.adExit(url);
+  } catch (e) {}
+  if (typeof clickTag == "function") clickTag.call(this, url);
+  else
+    throw new Error(
+      "ERROR: clickTag for this platform needs to be a callable function. See <Network> docs."
+    );
+};
