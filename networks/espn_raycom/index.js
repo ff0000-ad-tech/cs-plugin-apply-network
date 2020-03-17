c85a12cbc51b1fc0ad5d4be6d96583c32cb3a4f2
Greg Connell <987118+gmcdev@users.noreply.github.com>
2020-03-17 10:35:31 -0700

    Revert "espn testing added, raycom deleted"

--- networks/espn_raycom/index.js
+++ networks/espn_raycom/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-	'clicktags.js': 'standard',
+	'clicktags.js': 'espn_on_channel',
 	'exit_aux.js': 'standard',
-	'exit.js': 'standard',
+	'exit.js': 'espn_raycom',
 	'onload.js': 'standard',
 	'meta.html': null,
 	'script.html': null
